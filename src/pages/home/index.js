842ecaddc8e11b8fe4d75662c3cf76583dcc0bad
Romulo Alvernaz <127262652+RomuloAlvernaz@users.noreply.github.com>
2023-06-15 21:38:13 -0300

    Update index

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,17 +1,33 @@
 import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import { View, StyleSheet, Text, Image, TouchableOpacity } from 'react-native';
 
 const HomeScreen = () => {
   return (
     <View style={styles.container}>
-      <View style={styles.greenArea} />
+      <View style={styles.greenArea}>
+        <Image
+          source={require('../../../assets/logo.png')}
+          style={styles.logo}
+        />
+      </View>
       <View style={styles.whiteArea}>
-        <View style={styles.rectangle}>
-          <Text style={styles.rectangleText}>
-            Garrafas Pet, Tampinhas,{'\n'}
-            Latinhas, Óleo de Cozinha,{'\n'}
-            Pilhas e Baterias.
-          </Text>
+        <View style={styles.rectangleWrapper}>
+          <View style={styles.rectangle}>
+            <Text style={styles.rectangleText}>
+              Garrafas Pet, Tampinhas,{'\n'}
+              Latinhas, Óleo de Cozinha,{'\n'}
+              Pilhas e Baterias.
+            </Text>
+          </View>
+          <View style={styles.greenRectangle}>
+            <Text style={styles.greenRectangleText}>
+              Se você trabalha com reciclagem,{'\n'}
+              cadastre-se no botão abaixo, é grátis!
+            </Text>
+          </View>
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.buttonText}>Cadastro</Text>
+          </TouchableOpacity>
         </View>
       </View>
       <View style={styles.bottomBar}>
@@ -29,6 +45,14 @@ const styles = StyleSheet.create({
   greenArea: {
     flex: 2,
     backgroundColor: '#6DE398',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  logo: {
+    width: 150,
+    height: 150,
+    resizeMode: 'contain',
+    marginBottom: -30,
   },
   whiteArea: {
     flex: 8,
@@ -36,9 +60,16 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  rectangle: {
+  rectangleWrapper: {
     width: 350,
-    height: 170,
+    height: 250,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 20,
+  },
+  rectangle: {
+    width: '100%',
+    height: '60%',
     borderRadius: 20,
     backgroundColor: '#c4c6c9',
     justifyContent: 'center',
@@ -48,6 +79,35 @@ const styles = StyleSheet.create({
     color: '#0e642f',
     fontSize: 22,
     textAlign: 'center',
+  },
+  greenRectangle: {
+    marginTop: 20,
+    width: '80%',
+    height: '27%',
+    borderRadius: 0,
+    backgroundColor: '#6DE398',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  greenRectangleText: {
+    color: 'black',
+    fontSize: 15,
+    textAlign: 'center',
+  },
+  button: {
+    marginTop: 20,
+    width: '60%',
+    height: 50,
+    marginBottom: -110,
+    backgroundColor: '#6DE398',
+    borderRadius: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 18,
+    fontWeight: 'bold',
   },
   bottomBar: {
     height: '10%',
@@ -63,6 +123,4 @@ const styles = StyleSheet.create({
 });
 
 export default HomeScreen;
-
-
 
